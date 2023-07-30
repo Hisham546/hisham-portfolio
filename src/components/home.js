85f6d69ad7250087b263d4a4c7af71ab5aa18c48
Hisham <hmuhammed546@gmail.com>
2023-07-30 22:27:40 +0530

    adding elements

--- src/components/home.js
+++ src/components/home.js
@@ -9,7 +9,9 @@ return(
 
 <div className="mainContainer">
     <div className="headerContainer">
-    <h1 className="name">Hisham .</h1>
+    <h2 className="name">Hisham .</h2>
+    <h2 className="name">Works</h2>
+    <h2 className="name">About</h2>
     </div>
 
 </div>

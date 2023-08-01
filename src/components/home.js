19390823b952492da026cdaecc2a1c5fcda3aab1
Hisham <hmuhammed546@gmail.com>
2023-08-01 10:59:57 +0530

    added hover effects for top headings

--- src/components/home.js
+++ src/components/home.js
@@ -1,21 +1,22 @@
 import React  from "react";
+import {useState} from "react";
 import './styles/home.css'
 import { hover } from "@testing-library/user-event/dist/hover";
 import image from "./assets/images/pexels-otavio-fonseca-4665064.jpg"
 
 
 export function Home(){
-
+//const [color,setColor]=useState('')
 return(
 
 
 <div style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat",backgroundSize:"cover" }}
  className="mainContainer">
     <div className="headerContainer">
-    <h2 className="name">Home</h2>
-    <h2 className="name">About me </h2>
-    <h2 className="name">Portfolio</h2>
-    <h2 className="name">Contact </h2>   
+    <h2 className="topHeadings">Home</h2>
+    <h2 className="topHeadings">About me </h2>
+    <h2 className="topHeadings">Portfolio</h2>
+    <h2 className="topHeadings">Contact </h2>   
     </div>
    
     <h2  className="myName">Hello, i'm Muhammed Hisham </h2> 

3a386b7bc3bbd1fba197890d51567530e3414a22
Hisham <hmuhammed546@gmail.com>
2023-08-01 10:02:59 +0530

    changing ui

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React  from "react";
 import './styles/home.css'
+import { hover } from "@testing-library/user-event/dist/hover";
 
 
 export function Home(){
@@ -9,10 +10,13 @@ return(
 
 <div className="mainContainer">
     <div className="headerContainer">
-    <h2 className="name">Hisham .</h2>
-    <h2 className="name">Works</h2>
-    <h2 className="name">About</h2>
+    <h2 className="name">Home</h2>
+    <h2 className="name">About me </h2>
+    <h2 className="name">Portfolio</h2>
+    <h2 className="name">Contact </h2>   
     </div>
+   
+    <h2  className="myName">Hello, i'm Muhammed Hisham </h2> 
 
 </div>
 

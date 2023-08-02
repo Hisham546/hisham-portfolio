73e966e338b9b91806565e06ef3d39a6e288072f
Hisham <hmuhammed546@gmail.com>
2023-08-02 11:47:47 +0530

    added basic typed animation

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import './styles/home.css'
-
-
 import image from "./assets/images/abstract-textured-backgound.jpg"
-
+import { TypeAnimation } from 'react-type-animation';
 export function Home() {
-    //const [color,setColor]=useState('')
+
     return (
 
 
@@ -18,12 +16,32 @@ export function Home() {
                 <h2 className="topHeadings">Portfolio</h2>
                 <h2 className="topHeadings">Contact </h2>
             </div>
-            <h2 className="mobileApp"> Mobile App Developer </h2>
+    
             <div className="myNameDiv">
                 <h2 className="helloText">Hello,i'm  </h2>
                 <h2 className="myName"> Muhammed Hisham </h2>
+                <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        "Hello,i'm",
+        1000, // wait 1s before replacing "Mice" with "Hamsters"
+        ' Muhammed Hisham ',
+        1000,
+        'Application Developer',
+        1000,
+        'I create UI rich apps ',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      preRenderFirstString={true}
+      omitDeletionAnimation={false}
+      style={{ fontSize: '2em', display: 'inline-block',color:'white' }}
+      repeat={0}
+    />
             </div>
-            
+
+
         </div>
 
     )

b2ccf402825bfa3ed6cb6ffb68a27a7002e8e489
Hisham <hmuhammed546@gmail.com>
2023-08-02 10:48:17 +0530

    changed bg image

--- src/components/home.js
+++ src/components/home.js
@@ -1,32 +1,32 @@
-import React  from "react";
-import {useState} from "react";
+import React from "react";
+import { useState } from "react";
 import './styles/home.css'
-import { hover } from "@testing-library/user-event/dist/hover";
-import image from "./assets/images/pexels-otavio-fonseca-4665064.jpg"
 
 
-export function Home(){
-//const [color,setColor]=useState('')
-return(
+import image from "./assets/images/abstract-textured-backgound.jpg"
+
+export function Home() {
+    //const [color,setColor]=useState('')
+    return (
 
 
-<div style={{ backgroundImage:`url(${image})`,backgroundRepeat:"no-repeat",backgroundSize:"cover" }}
- className="mainContainer">
-    <div className="headerContainer">
-    <h2 className="topHeadings">Home</h2>
-    <h2 className="topHeadings">About me </h2>
-    <h2 className="topHeadings">Portfolio</h2>
-    <h2 className="topHeadings">Contact </h2>   
-    </div>
-   <div className="myNameDiv">
-    <h2  className="helloText">Hello,i'm  </h2> 
-    <h2  className="myName"> Muhammed Hisham </h2> 
-  
-    </div>
-    <h2  className="mobileApp"> Mobile App Developer </h2>   
-</div>
+        <div style={{ backgroundImage: `url(${image})`, backgroundRepeat: "no-repeat", backgroundSize: "cover" }}
+            className="mainContainer">
+            <div className="headerContainer">
+                <h2 className="topHeadings">Home</h2>
+                <h2 className="topHeadings">About me </h2>
+                <h2 className="topHeadings">Portfolio</h2>
+                <h2 className="topHeadings">Contact </h2>
+            </div>
+            <h2 className="mobileApp"> Mobile App Developer </h2>
+            <div className="myNameDiv">
+                <h2 className="helloText">Hello,i'm  </h2>
+                <h2 className="myName"> Muhammed Hisham </h2>
+            </div>
+            
+        </div>
 
-)
+    )
 
 
 }

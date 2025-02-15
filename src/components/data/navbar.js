2c73c137ad8674259dc3497e33b4ddf1f89e6009
Muhammed Hisham <hmuhammed546@gmail.com>
2025-02-15 15:39:10 +0530

    modifications for mobile view

--- src/components/data/navbar.js
+++ src/components/data/navbar.js
@@ -15,8 +15,11 @@ import About from "../../presentation/about/about";
 import { Link } from "react-scroll";
 import UseAnimations from "react-useanimations";
 import activity from 'react-useanimations/lib/activity';
+
+import Hamburger from "hamburger-react";
 function NavBar() {
   const [menu, setMenu] = useState("home")
+  const [toggle, setToggle] = useState(false);
   const links = [
     {
       name: "Home",
@@ -42,10 +45,13 @@ function NavBar() {
       className='navbar'>
       <div className="decorIcon">
       </div>
-      <ul
-        className='navbar-menu'
+      {/* Hamburger Menu Button (Visible only on mobile) */}
+      <div className="md:hidden flex items-center hamburger-menu">
+        <Hamburger toggled={toggle} toggle={setToggle} size={22} color="#000000" />
+      </div>
+      {/* Navbar Links (Visible in Desktop and toggles in Mobile) */}
+      <ul className={`navbar-menu ${toggle ? "active" : "hidden"} md:flex`}>
 
-      >
         {links.map((el) => (
           <li className="cursor-pointer">
             <Link
@@ -54,6 +60,7 @@ function NavBar() {
               spy={true}
               smooth={true}
               className="topHeadings text-base sm:text-lg md:text-xl lg:text-2xl"
+              onClick={() => setToggle(false)} // Closes menu when clicking a link
             >
               {el.name}
             </Link>
